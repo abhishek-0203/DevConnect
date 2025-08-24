8f61c84880c2015beaf24ed043c411e5cfb807a2
Ashwani Kumar Singh <ashwanisingh8713@gmail.com>
2025-08-24 22:58:35 +0530

    Added Chat page - 2

---
+++ site/src/jvmMain/kotlin/com/example/blogmultiplatform/api/Chat.kt
@@ -0,0 +1,20 @@
+package com.example.blogmultiplatform.api
+
+import com.example.shared.Message
+import com.varabyte.kobweb.api.stream.ApiStream
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+val chat = ApiStream { ctx ->
+    try {
+        val message = Json.decodeFromString<Message>(ctx.text)
+        if (message.username.isNotBlank() && message.text.isNotBlank()) {
+            val jsonMessage = Json.encodeToString(message)
+            ctx.stream.broadcast(jsonMessage) { true }
+        }
+        // Optionally, handle invalid messages (e.g., log or ignore)
+    } catch (_: Exception) {
+        // Optionally, handle deserialization errors (e.g., log or ignore)
+    }
+}
